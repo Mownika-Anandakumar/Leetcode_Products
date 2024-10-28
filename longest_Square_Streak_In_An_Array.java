079c216cfdc627e9037d9c8b88e5746f7fa29a91
Mownika Anandakumar <165825348+Mownika-Anandakumar@users.noreply.github.com>
2024-10-28 22:27:22 +0530

    Create longest_Square_Streak_In_An_Array.java

---
+++ longest_Square_Streak_In_An_Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+        Map<Integer, Integer> nextSquareToLength = new HashMap<>();
+        Arrays.sort(nums);
+        int maxLength = 0;
+        for (int num : nums) {
+            if (nextSquareToLength.containsKey(num)) {
+                int val = nextSquareToLength.get(num) + 1;
+                nextSquareToLength.put(num*num, val);
+                if (val > maxLength) {
+                    maxLength = val;
+                }
+            } else {
+                nextSquareToLength.put(num*num, 1);
+            }
+        }
+        return (maxLength > 1) ? maxLength : -1;
+    }
+}
